fc845943b9ed107e279b71b2d73c73d2ea66bcf7
JackTrusler <jtrus93@gmail.com>
2023-03-19 13:04:01 -0400

    desc

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 const bot = new Telegraf(process.env.API_TOKEN);
 
+/*
+  This command generates a fair coin flip using Javascript's built-in Math.random() function, the 
+  probability of getting 'heads' or 'tails' is evenly distributed 50/50.
+
+  This command is useful for gamers that want a quick way to flip a coin, without needing a pocket Abe. 
+  */
 bot.command('flip', ctx => {
   bot.telegram.sendMessage(ctx.chat.id, Math.random() < 0.5 ? 'heads' : 'tails');
 })
